5acb75690853fe99e8229e88c8f44d395945a620
dojo2023-Takemura <130328078+dojo2023-Takemura@users.noreply.github.com>
2023-06-14 17:34:24 +0900

    Edit: APIテストのために変更

--- src/src/servlet/HomeServlet.java
+++ src/src/servlet/HomeServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.UsageTakemura;
+
 /**
  * Servlet implementation class HomeServlet
  */
@@ -43,23 +45,23 @@ public class HomeServlet extends HttpServlet {
 		// APIのテスト
 		// 他の機能が実装できてからこちらを組み込みます！
 //		HttpRequest navRequest = HttpRequest.newBuilder()
-//				.uri(URI.create("https://navitime-route-totalnavi.p.rapidapi.com/route_transit?start=00004254&goal=00000148&start_time=2023-06-14T12%3A30%3A00&datum=wgs84&term=1440&limit=5&coord_unit=degree"))
+//				.uri(URI.create("https://navitime-transport.p.rapidapi.com/transport_node/around?coord=" + "35.70172838341061%2C139.67179608666535" + "&limit=1"))
 //				.header("X-RapidAPI-Key", "42fbfc38f7msh32b35a875763945p123a2cjsn5122a195ca21")
-//				.header("X-RapidAPI-Host", "navitime-route-totalnavi.p.rapidapi.com")
+//				.header("X-RapidAPI-Host", "navitime-transport.p.rapidapi.com")
 //				.method("GET", HttpRequest.BodyPublishers.noBody())
 //				.build();
 //		HttpResponse<String> navResponse = null;
 //		try {
 //			navResponse = HttpClient.newHttpClient().send(navRequest, HttpResponse.BodyHandlers.ofString());
-//		} catch (IOException e) {
-//			// TODO 自動生成された catch ブロック
-//			e.printStackTrace();
-//		} catch (InterruptedException e) {
+//		} catch (IOException | InterruptedException e) {
 //			// TODO 自動生成された catch ブロック
 //			e.printStackTrace();
 //		}
 //		System.out.println(navResponse.body());
-//
+
+		String stationId = UsageTakemura.convertGeoToId("35.70172838341061,139.67169333390262");
+//		String stationName = UsageTakemura.convertIdToName(stationId);
+		System.out.println(stationId + "\n");
 		// TODO LastTrainServletにリダイレクトするように変更
 		response.sendRedirect("/syuudeen/HomeServlet");
 	}
