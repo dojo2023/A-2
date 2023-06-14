db5624c6f60ad073555efcc5d6abd28f8c539dba
dojo2023-aiba <aibataeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-14 11:23:37 +0900

    お豆たちを変更しました

---
+++ src/src/model/AllBeans.java
@@ -0,0 +1,122 @@
+package model;
+
+public class AllBeans {
+	private String lastTrainId;
+	private String startTime;
+	private String overFlag;
+	private String goalTime;
+	private String planId;
+	private String planDate;
+	private String planName;
+	private String place;
+	private String remarks;
+	private String userId;
+	private String userPw;
+	private String stationHome;
+	private String userAlert;
+
+	public String getLastTrainId() {
+		return lastTrainId;
+	}
+
+	public void setLastTrainId(String lastTrainId) {
+		this.lastTrainId = lastTrainId;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getOverFlag() {
+		return overFlag;
+	}
+
+	public void setOverFlag(String overFlag) {
+		this.overFlag = overFlag;
+	}
+
+	public String getGoalTime() {
+		return goalTime;
+	}
+
+	public void setGoalTime(String goalTime) {
+		this.goalTime = goalTime;
+	}
+
+	public String getPlanId() {
+		return planId;
+	}
+
+	public void setPlanId(String planId) {
+		this.planId = planId;
+	}
+
+	public String getPlanDate() {
+		return planDate;
+	}
+
+	public void setPlanDate(String planDate) {
+		this.planDate = planDate;
+	}
+
+	public String getPlanName() {
+		return planName;
+	}
+
+	public void setPlanName(String planName) {
+		this.planName = planName;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPw() {
+		return userPw;
+	}
+
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	public String getStationHome() {
+		return stationHome;
+	}
+
+	public void setStationHome(String stationHome) {
+		this.stationHome = stationHome;
+	}
+
+	public String getUserAlert() {
+		return userAlert;
+	}
+
+	public void setUserAlert(String userAlert) {
+		this.userAlert = userAlert;
+	}
+
+}
