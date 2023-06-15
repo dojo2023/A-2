6361cfb08780880fcfdd8ebe96317dab794705da
dojo2023-sato <130334455+dojo2023-sato@users.noreply.github.com>
2023-06-15 11:08:53 +0900

    ホームサーブレットにメソッド追加
    通知変更に必要な値を追加したよ

--- src/src/servlet/HomeServlet.java
+++ src/src/servlet/HomeServlet.java
@@ -8,6 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UsersDao;
+
+
+
 
 /**
  * Servlet implementation class HomeServlet
@@ -40,6 +46,27 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		// 送信されたデータの取得
+		String check = request.getParameter("check");
+		//userIDを取得
+		HttpSession session = request.getSession();
+		String userId = (String)session.getAttribute("userId");
+
+		//DAOを実体化（インスタンス化）
+		UsersDao usersdao = new UsersDao();
+
+		//DAOのメソッドに引数を２つ渡してアップデートを行う
+		int result = usersdao.checkUpdate(check,userId);
+
+		if(result==0) {
+			request.setAttribute("msg", "失敗したよ");
+		}else {
+			request.setAttribute("msg", "成功したよ");
+		}
+
+		//ここまで来たらjspに勝手に処理が戻る
+		//${msg}
 
 		// APIのテスト
 //		String stationId = UsageTakemura.convertGeoToId("35.70172838341061,139.67169333390262");
