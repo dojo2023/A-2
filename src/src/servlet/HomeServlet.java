ee354ca9a80b87e4f832d06c816c478e1e20c41e
dojo2023-Takemura <130328078+dojo2023-Takemura@users.noreply.github.com>
2023-06-16 17:03:45 +0900

    Edit: ログイン判定部のレベルを変更

--- src/src/servlet/HomeServlet.java
+++ src/src/servlet/HomeServlet.java
@@ -70,31 +70,32 @@ public class HomeServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+	// もしもログインしていなかったらログインサーブレットにリダイレクトする
+	// ！！！ Cookieを使用 ！！！
+	Cookie cookie[] = request.getCookies();		// Cookieは複数ある可能性があるため配列
+	String userId = null;			// userIdが保存されていたらその値、なければnull
+
+	if (cookie != null){
+		for (int i = 0 ; i < cookie.length ; i++){
+			if (cookie[i].getName().equals("userId")){
+				userId = cookie[i].getValue();
+				break;
+			}
+		}
+	}
+
+	if (userId == null) {
+		response.sendRedirect("/syuudeen/LoginServlet");
+		return;
+	}
+	// 以上ログインの確認
+
 	if (request.getParameter("SUBMIT") == null) {
 		// 送信されたデータの取得
 		String check = request.getParameter("check");
 
 		System.out.print(check);//テスト用
 
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		// ！！！ Cookieを使用 ！！！
-		Cookie cookie[] = request.getCookies();		// Cookieは複数ある可能性があるため配列
-		String userId = null;			// userIdが保存されていたらその値、なければnull
-
-		if (cookie != null){
-			for (int i = 0 ; i < cookie.length ; i++){
-				if (cookie[i].getName().equals("userId")){
-				userId = cookie[i].getValue();
-				break;
-	        }
-	      }
-	    }
-
-		if (userId == null) {
-			response.sendRedirect("/syuudeen/LoginServlet");
-			return;
-		}
-		// 以上ログインの確認
 
 
 		//DAOを実体化（インスタンス化）
