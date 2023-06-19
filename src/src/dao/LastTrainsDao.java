4cc57167c343342834d2140ff7561bc609fc683d
dojo2023-sato <130334455+dojo2023-sato@users.noreply.github.com>
2023-06-19 17:09:40 +0900

    update追加したよ

--- src/src/dao/LastTrainsDao.java
+++ src/src/dao/LastTrainsDao.java
@@ -5,6 +5,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -36,12 +40,12 @@ public class LastTrainsDao {
 
 		Calendar now = Calendar.getInstance(); //現在時刻
 		Calendar cn1 = Calendar.getInstance(); //午前2時を超えたか判定するための時刻
-		
+
 		//位置情報検索ボタンを押した日の午前2時に日付を設定する
 		cn1.set(now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DATE),2,0,0);
-		
+
 		if( now.compareTo(cn1)>=0) { //nowを6月19日17：00  cn1を6月19日02：00とした場合"1"を返す
-			
+
 			//その日の終電 6月19日19時の場合 到着時間を6月20日2時に設定
 			cn1.set(now.get(Calendar.YEAR),now.get(Calendar.MONTH),now.get(Calendar.DATE)+1,2,0,0);
 		}
@@ -71,5 +75,77 @@ public class LastTrainsDao {
 		}
 
 		return navResponse.body();
+	}
+
+	public boolean update(String lastTrainId, String startTime, String goalTime, String overFlag, String userId) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/src/data/syuudeen", "sa", "");
+
+			// SQL文を準備する
+			String sql = "update LAST_TRAINS set LAST_TRAIN_ID = ? START_TIME = ?, GOAL_TIME = ?, OVER_FLAG = ?, where USER_ID = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			if(!lastTrainId.equals("")) {
+				pStmt.setString(1, lastTrainId);
+			}
+			else {
+				pStmt.setString(1, null);
+			}
+			if(!startTime.equals("")) {
+				pStmt.setString(2, startTime);
+			}
+			else {
+				pStmt.setString(2, null);
+			}
+			if(!goalTime.equals("")) {
+				pStmt.setString(3, goalTime);
+			}
+			else {
+				pStmt.setString(3, null);
+			}
+
+			if(!overFlag.equals("")) {
+				pStmt.setString(4, overFlag);
+			}
+			else {
+				pStmt.setString(4, null);
+			}
+
+			pStmt.setString(5, userId);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+
+		return result;
+
 	}
 }
