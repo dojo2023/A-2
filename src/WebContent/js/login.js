979f8882e7905c5e3648b3ebc8df1ee0f98227c8
dojo2023-miyano <130334604+dojo2023-miyano@users.noreply.github.com>
2023-06-21 15:23:19 +0900

    ログインチェックアイコン

--- src/WebContent/js/login.js
+++ src/WebContent/js/login.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+
+      function pushHideButton() {
+        var txtPass = document.getElementById("user_pw");
+        var btnEye = document.getElementById("pw_check_button");
+        if (txtPass.type === "text") {
+          txtPass.type = "password";
+          btnEye.className = "fa fa-eye";
+        } else {
+          txtPass.type = "text";
+          btnEye.className = "fa fa-eye-slash";
+        }
+      }
