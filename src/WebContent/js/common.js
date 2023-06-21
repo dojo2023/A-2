1b3242b09a6a9cbc5696e94a41c00bd75e1c729c
dojo2023-kubo-yuki <130334659+dojo2023-kubo-yuki@users.noreply.github.com>
2023-06-21 15:00:18 +0900

    humberger.js修正

--- src/WebContent/js/common.js
+++ src/WebContent/js/common.js
@@ -2,9 +2,9 @@ const MenuContainer = document.querySelector('.menu_container');
 const MenuOpen = document.querySelector('.menu_open');
 const MenuClose = document.querySelector('.menu_close');
 
-const Menu = document.querySelector('.menu');
+const MenuList = document.querySelector('.menu_list');
 	MenuContainer.addEventListener('click', () => {
 		MenuOpen.classList.toggle('active');
 		MenuClose.classList.toggle('active');
-		Menu.classList.toggle('active');
+		MenuList.classList.toggle('active');
 	});
