ed395e0b2b2c60dc7f0f540b95223f0a6152523c
dojo2023-kubo-yuki <130334659+dojo2023-kubo-yuki@users.noreply.github.com>
2023-06-23 13:08:16 +0900

    最寄り駅変更画面toggle完成

--- src/WebContent/js/toggle.js
+++ src/WebContent/js/toggle.js
@@ -1,13 +1,16 @@
 /**
- * 
+ *
  */
 
 //スイッチの外枠とスイッチの要素を取得
 const toggle = document.querySelector(".toggle");
 const toggleSwitch = document.querySelector(".toggle_switch");
+const check = document.getElementById("alert_check");
 
 //クリックでacitveクラスを追加/削除
 toggle.addEventListener("click", () => {
+	check.checked = !check.checked;
+	check.onchange();
     toggle.classList.toggle("active");
     toggleSwitch.classList.toggle("active");
 });
